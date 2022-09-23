c34d437b84698a4bc733de6e40e242a98fc15496
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2022-09-23 19:21:32 +0100

    Update index.php

--- month2/week1/cbt/app/views/index.php
+++ month2/week1/cbt/app/views/index.php
@@ -28,7 +28,7 @@
                   <div class="text-center">
                     <button type="submit" class="btn btn-primary btn-block enter-btn">Login</button>
                   </div>
-                  <p class="sign-up">Don't have an Account?<a href="#"> Sign Up</a></p>
+                  <p class="sign-up">Don't have an Account?<a href="<? echo URLROOT; ?>/Users/register"> Sign Up</a></p>
                 </form>
               </div>
             </div>
